3b38b792fc7f09bd4b8019782ffcc7d861173084
karenwsit <karenwsit@gmail.com>
2016-01-02 18:20:56 -0800

    added twitter OAuth process

--- scripts/twitter_streaming.py
+++ scripts/twitter_streaming.py
@@ -2,3 +2,6 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+# twitter OAuth process
+auth = OAuthHandler(os.environ.get('TWITTER_CONSUMER_KEY'), os.environ.get('TWITTER_CONSUMER_SECRET'))
+auth.set_access_token(os.environ.get('TWITTER_ACCESS_TOKEN'), os.environ.get('TWITTER_ACCESS_TOKEN_SECRET'))
